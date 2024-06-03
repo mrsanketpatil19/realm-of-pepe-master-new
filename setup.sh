55b240e5760f2a7b49a77a52301440f2f8e0b2a4
Mrunali Patil <mrunalipatil@loop-depaulsecure-253-118.depaulsecure-student.depaul.edu>
2024-06-03 15:23:05 -0500

    Update setup.sh to install gimme and set Go version, update netlify.toml

--- setup.sh
+++ setup.sh
@@ -8,6 +8,15 @@ apt-get update && apt-get upgrade -y
 # Install missing packages
 apt-get install -y coreutils procps
 
+# Install gimme for managing Go versions
+curl -sL -o gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+chmod +x gimme
+./gimme 1.19
+
+# Set up Go environment
+export GOROOT=$HOME/.gimme/versions/go1.19.linux.amd64
+export PATH=$GOROOT/bin:$PATH
+
 # Install Foundry and Forge
 if ! command -v forge &> /dev/null; then
     echo "Foundry is not installed. Installing..."
