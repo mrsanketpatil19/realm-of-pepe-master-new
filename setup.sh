e431731d873575d24835b1327f2ac865ef5bcf0c
Mrunali Patil <mrunalipatil@Mrunalis-MacBook-Air.local>
2024-06-02 22:31:27 -0500

    Update setup.sh and netlify.toml to install forge and foundryup

--- setup.sh
+++ setup.sh
@@ -6,12 +6,9 @@ if ! command -v forge &> /dev/null
 then
     echo "Foundry is not installed. Installing..."
     curl -L https://foundry.paradigm.xyz | bash
+    source ~/.bashrc
     foundryup
 fi
 
 # Install dependencies
 pnpm install
-
-
-
-
